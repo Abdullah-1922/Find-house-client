ebf0a692dab078ef7120550daf7940d53e06e99f
kazirauf <raufelahi9876@gmail.com>
2024-11-08 00:55:41 +0600

    the Why Choose Us section in home page is ready

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Home/Banner/Banner";
 import RecentProperties from "@/components/Home/RecentProperties/RecentProperties";
+import WhyChooseUs from "@/components/Home/WhyChooseUs/WhyChooseUs";
 
 
 
@@ -8,6 +9,7 @@ export default function Home() {
  <div>
     <Banner></Banner>
     <RecentProperties></RecentProperties>
+    <WhyChooseUs></WhyChooseUs>
  </div>
   );
 }
