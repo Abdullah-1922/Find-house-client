9c4bb8a6bc50a53db24032701edbd6a5f048facd
kazirauf <raufelahi9876@gmail.com>
2024-11-09 15:55:42 +0600

    agent section is done in home page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 import Apartmentforrent from "@/components/Home/Apartmentforrent/Apartmentforrent";
 import Banner from "@/components/Home/Banner/Banner";
+import MeetOurAgents from "@/components/Home/MeetOurAgents/MeetOurAgents";
 import PopularPlaces from "@/components/Home/PopularPlaces/PopularPlaces";
 import RecentProperties from "@/components/Home/RecentProperties/RecentProperties";
 import WhyChooseUs from "@/components/Home/WhyChooseUs/WhyChooseUs";
@@ -14,6 +15,7 @@ export default function Home() {
     <WhyChooseUs/>
     <PopularPlaces/>
     <Apartmentforrent/>
+    <MeetOurAgents/>
  </div>
   );
 }
