0ee57b85fe279495afe6d0d467ac7fe5d262e4de
MacBook Pro <naym100m@gmail.com>
2024-11-09 16:26:31 +0600

    [created] a PropertySearchForm use react hook form

--- src/app/(commonLayout)/home-map/_components/IndustryLocation.tsx
+++ src/app/(commonLayout)/home-map/_components/IndustryLocation.tsx
@@ -1,27 +1,56 @@
 "use client";
 
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useCallback, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import PropertySearchForm from "./PropertySearchForm";
 
-const IndustryLocation = () => {
-  const containerStyle = {
-    width: "100%",
-    height: "80vh",
-  };
+const containerStyle = {
+  width: "100%",
+  height: "100vh",
+};
 
-  const center = {
-    lat: -34.397,
-    lng: -150.644,
-  };
-  return (
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+const IndustryLocation: React.FC = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY", // Replace with your actual API key
+  });
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  //   console.log(map);
+
+  const onLoad = useCallback((map: google.maps.Map) => {
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
     <>
-      <LoadScript googleMapsApiKey="">
-        <GoogleMap 
-        mapContainerStyle={containerStyle} 
-        center={center} 
-        zoom={14}>
-          <></>
-        </GoogleMap>
-      </LoadScript>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+      </GoogleMap>
+      <div className=" absolute top-32 right-20">
+        <PropertySearchForm />
+      </div>
+    </>
+  ) : (
+    <>
+      <div className=" flex items-center justify-center h-screen">
+        <p>Loading...</p>
+      </div>
     </>
   );
 };
