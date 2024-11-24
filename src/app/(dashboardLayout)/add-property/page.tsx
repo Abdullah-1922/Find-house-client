8f3c974a20f882d037aab48a4277eff51f0b8d93
Junayed Noman <junayednoman05@gmail.com>
2024-11-24 18:47:44 +0600

    feat: all property table and pagination retrieved

--- src/app/(dashboardLayout)/add-property/page.tsx
+++ src/app/(dashboardLayout)/add-property/page.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React from "react";
+import AddProperties from "../_components/modules/allProperties/addProperties";
 
 export default function AddPropertyPage() {
-  return <div>AddPropertyPage</div>;
+  return (
+    <div>
+      <AddProperties />
+    </div>
+  );
 }
