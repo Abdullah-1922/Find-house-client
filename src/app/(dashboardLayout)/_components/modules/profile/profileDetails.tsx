a41729440b3fdc1115c2608fbc62c37ac9197dc7
Abdullah-1922 <kafikafi1922@gmail.com>
2024-12-15 15:29:40 +0600

    request for agent feature running

--- src/app/(dashboardLayout)/_components/modules/profile/profileDetails.tsx
+++ src/app/(dashboardLayout)/_components/modules/profile/profileDetails.tsx
@@ -8,10 +8,18 @@ import { useUser } from "@/hooks/user.hook";
 import { InquiryForm } from "./inwueryForm";
 import Link from "next/link";
 import Spinner from "@/components/ui/spinner";
+import {
+  useApplyForAgentMutation,
+  useGetUserAgentRequestQuery,
+} from "@/redux/api/features/users/requestAgentApi";
 
 export default function ProfileDetail() {
   const { user } = useUser();
-
+  const { data, isLoading } = useGetUserAgentRequestQuery(user?._id);
+  const [applyForAgent] = useApplyForAgentMutation();
+  console.log(user?._id);
+  if (isLoading) return <Spinner />;
+  console.log(data);
   if (!user) {
     return (
       <Card className="w-full mx-auto">
@@ -25,7 +33,15 @@ export default function ProfileDetail() {
       </Card>
     );
   }
-  const isLoading = false;
+
+  const handleApplyForAgent = async () => {
+    const body = {
+      userId: user._id,
+    };
+    const res = await applyForAgent(body);
+    console.log(res?.data);
+  };
+
   const {
     firstName,
     secondName,
@@ -88,24 +104,38 @@ export default function ProfileDetail() {
           <div>
             {user?.role === "user" && (
               <div className="flex flex-col mt-10 items-center justify-between w-full">
-                <p className="text-sm font-semibold mb-2 text-red-600">
-                  Once you become an agent, you can add properties.
-                </p>
-                <Button
-                  type="submit"
-                  className="w-full  bg-gray-800 hover:bg-gray-900"
-                >
-                  {isLoading ? (
-                    <div>
-                      <div className="flex items-center gap-2">
-                        <span>Submitting...</span>{" "}
-                        <Loader className="mr-2 h-4 w-4 animate-spin" />
-                      </div>
-                    </div>
-                  ) : (
-                    " Request for be agent"
-                  )}
-                </Button>
+                {(!data?.data || data?.data?.status === "rejected") && (
+                  <>
+                    <p className="text-sm font-semibold mb-2 text-red-600">
+                      {data?.data?.status === "rejected"
+                        ? "Your request for agent has been rejected. Please try again."
+                        : "You are not an agent yet. Please apply to be an agent."}
+                    </p>
+                    <Button
+                      onClick={handleApplyForAgent}
+                      type="submit"
+                      className="w-full  bg-gray-800 hover:bg-gray-900"
+                    >
+                      {isLoading ? (
+                        <div>
+                          <div className="flex items-center gap-2">
+                            <span>Submitting...</span>
+                            <Loader className="mr-2 h-4 w-4 animate-spin" />
+                          </div>
+                        </div>
+                      ) : (
+                        " Apply for be agent"
+                      )}
+                    </Button>
+                  </>
+                )}
+
+                {data?.data?.status === "pending" && (
+                  <p className="text-sm font-semibold mb-2 text-red-600">
+                    Your request for agent is pending. Please wait for admin
+                    approval.
+                  </p>
+                )}
               </div>
             )}
           </div>
